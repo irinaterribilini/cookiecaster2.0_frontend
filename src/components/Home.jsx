459477811dbb5f2069794158bd178b9f280b5d31
irinaterribilini <irina.terribilini@students.fhnw.ch>
2018-03-31 15:42:59 +0200

    styling and more

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,18 +9,19 @@ export default class Home extends Component {
         return (
             <Grid>
                 <Jumbotron>
+                <Row>
+                    <Col xs={12} sm={8} className="main-section">
                     <h2>Willkommen beim Cookiecaster 2.0</h2>
                     <p>Möchtest Du Deine eigene persönliche Keksform kreieren?</p>
                     <Link to="/start">
                         <Button bsStyle="primary">Los geht's!</Button>
                     </Link>
-                </Jumbotron>
-<Col>
-    <Row>
-        
-    </Row>
-</Col>
-
+                    </Col>
+                    <Col xs={12} sm={4} className="main-section">
+                        <Image src="assets/Pfote.png" responsive />
+                    </Col>
+                </Row>
+            </Jumbotron>
             </Grid>
         )
     }
