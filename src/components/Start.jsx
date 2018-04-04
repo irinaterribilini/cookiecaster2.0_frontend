1c25731fdc33012a36b4623925e76f94501c94a2
irinaterribilini <irina.terribilini@students.fhnw.ch>
2018-04-04 14:38:00 +0200

    added save button

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -10,8 +10,9 @@ export default class Start extends Component {
                 <svg class="col-md-6" width="800" height="600" id="drawingarea">
                 </svg>
 
-                <button onclick="graph.clearCanvas()" class="btn btn-primary pull-right" id="clear">Clear</button>
-                <button onclick="graph.createMesh()" class="btn btn-primary pull-right" id="createMesh">CreateMesh</button>
+                    <button onclick="graph.clearCanvas()" class="btn btn-primary pull-right" id="clear">Clear</button>
+                    <button onclick="graph.createMesh()" class="btn btn-primary pull-right" id="createMesh">CreateMesh</button>
+                    <button onclick="graph.save()" class="btn btn-primary pull-right" id="save">Save</button>
                 </Grid>
             </div>
         )
